625ad7e0924bc59ab53e465176fe288f70e8e596
Jared Bruni <jaredbruni@gmail.com>
2023-07-09 23:35:41 -0700

    interface

--- glitch.gui/new_image.cpp
+++ glitch.gui/new_image.cpp
@@ -2,9 +2,29 @@
 #include"main_window.hpp"
 
 NewImageWindow::NewImageWindow(QWidget *parent) : QDialog(parent) {
+    setFixedSize(320, 240);
+    input_file = new QLabel(tr("Select File Location"), this);
+    input_file->setGeometry(10, 10, 250, 25);
+    input_file_select = new QPushButton("...", this);
+    input_file_select->setGeometry(260, 10, 50, 25);
+    connect(input_file_select, SIGNAL(clicked()), this, SLOT(openFile()));
+    output_location = new QLabel(tr("Select Output Location"), this);
+    output_location->setGeometry(10, 35, 250, 25);
+    output_location_select = new QPushButton("...", this);
+    output_location_select->setGeometry(260, 35, 50, 25);
+    connect(output_location_select, SIGNAL(clicked()), this, SLOT(selectDir()));
+
 
 }
 
 void NewImageWindow::setMainWindow(MainWindow *main_w) {
     main_window = main_w;
+}
+
+void NewImageWindow::openFile() {
+
+}
+
+void NewImageWindow::selectDir() {
+
 }
--- glitch.gui/new_image.hpp
+++ glitch.gui/new_image.hpp
@@ -2,6 +2,9 @@
 #define _NEW_IMAGE_H_
 
 #include<QDialog>
+#include<QLabel>
+#include<QPushButton>
+#include<QLineEdit>
 
 class MainWindow;
 
@@ -12,7 +15,16 @@ public:
     void setMainWindow(MainWindow *main_w);
 private:
     MainWindow *main_window;
+    QLabel *input_file;
+    QPushButton *input_file_select;
+    QLabel *output_location;
+    QPushButton *output_location_select;
+    QLineEdit *video_fps;
+    QPushButton *video_start;
 
+public slots:
+    void openFile();
+    void selectDir();
 };
 
 #endif
