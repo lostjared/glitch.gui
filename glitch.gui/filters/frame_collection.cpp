7549c71c639f1edfc75ce6cad00cce777411efb4
Jared Bruni <jaredbruni@gmail.com>
2023-07-29 02:50:13 -0700

    frame_collection

--- glitch.gui/filters/frame_collection.cpp
+++ glitch.gui/filters/frame_collection.cpp
@@ -1,2 +1,28 @@
 #include"frame_collection.hpp"
 
+FrameCollection::FrameCollection(size_t max) {
+    setMaxFrames(max);
+}
+
+void FrameCollection::shiftFrames(cv::Mat &frame) {
+    if(frames.size() < max_frames) {
+        for(size_t i = 0; i < max_frames; ++i) {
+            frames.push_back(frame);
+        }
+        return;
+    }
+    frames.push_back(frame);
+    frames.pop_front();
+}
+
+const cv::Mat &FrameCollection::operator[](size_t pos) const {
+    return frames[pos];
+}
+ 
+cv::Mat &FrameCollection::operator[](size_t pos) {
+    return frames[pos];
+}
+  
+void FrameCollection::setMaxFrames(size_t max) {
+    max_frames = max;
+}
--- glitch.gui/filters/frame_collection.hpp
+++ glitch.gui/filters/frame_collection.hpp
@@ -1,6 +1,20 @@
 #ifndef _FRAME_COL_H_
 #define _FRAME_COL_H_
 
+#include<deque>
+#include"acidcam/ac.h"
+
+class FrameCollection {
+public:
+    FrameCollection(size_t max);
+    void shiftFrames(cv::Mat &frame);
+    const cv::Mat &operator[](size_t pos) const;
+    cv::Mat &operator[](size_t pos);
+    void setMaxFrames(size_t max);
+private:
+    size_t max_frames;
+    std::deque<cv::Mat> frames;
+};
 
 
 #endif
