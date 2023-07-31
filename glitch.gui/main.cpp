7cead7addcf19eb7153da507045e8ccf6c64a139
Jared Bruni <jaredbruni@gmail.com>
2023-07-31 03:55:52 -0700

    output name/version

--- glitch.gui/main.cpp
+++ glitch.gui/main.cpp
@@ -4,6 +4,7 @@
 
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
+    std::cout << APP_NAME << " v" << APP_VERSION << "\n";
     MainWindow main_window;
     main_window.show();
     app.exec();
