5095007b9b2a0737c986494ded1269bf23cfecae
Jared Bruni <jaredbruni@gmail.com>
2023-08-01 10:38:48 -0700

    Line_Down_X3

--- glitch.gui/filters/glitch.cpp
+++ glitch.gui/filters/glitch.cpp
@@ -52,3 +52,31 @@ void Glitch_Line_Down_X2::proc(cv::Mat &frame) {
 void Glitch_Line_Down_X2::clear() {}
 
 Glitch_Line_Down_X2::~Glitch_Line_Down_X2() {}
+
+/* line down 3 */
+
+void Glitch_Line_Down_X3::init() {
+    offset_y = 0;
+    color = cv::Vec3b(0, 0, 0);
+}
+
+void Glitch_Line_Down_X3::proc(cv::Mat &frame) {
+    for(int x = 0; x < frame.cols; ++x) {
+        color = cv::Vec3b(rand()%255, rand()%255, rand()%255);
+        for(int y = 0; y < offset_y && y < frame.rows; ++y) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(y, x);
+            pixel[0] += pixel[0]^color[0];
+            pixel[1] += pixel[1]^color[1];
+            pixel[2] += pixel[2]^color[2];
+        }
+        for(int y = offset_y; y < frame.rows; ++y) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(y, x);
+            pixel[0] = pixel[0]&color[0];
+            pixel[1] = pixel[1]&color[1];
+            pixel[2] = pixel[2]&color[2];
+        }
+        offset_y = rand()%frame.rows;
+    }
+}
+void Glitch_Line_Down_X3::clear() {}
+Glitch_Line_Down_X3::~Glitch_Line_Down_X3() {}
--- glitch.gui/filters/glitch.hpp
+++ glitch.gui/filters/glitch.hpp
@@ -26,4 +26,16 @@ private:
     cv::Vec3b color;
 };
 
+class Glitch_Line_Down_X3 : public FilterFunc {
+public:
+    void init() override;
+    void proc(cv::Mat &frame) override;
+    void clear() override;
+    ~Glitch_Line_Down_X3();
+private:
+    int offset_y;
+    cv::Vec3b color;
+
+};
+
 #endif
