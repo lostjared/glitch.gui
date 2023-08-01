142812a91c759d8b929111de2aae692550397082
Jared Bruni <jaredbruni@gmail.com>
2023-08-01 07:02:41 -0700

    random seed

--- glitch.gui/main.cpp
+++ glitch.gui/main.cpp
@@ -1,9 +1,13 @@
 #include<QApplication>
 #include"main_window.hpp"
+#include<ctime>
+#include<cstdlib>
 
 
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
+    // init random seed
+    srand(static_cast<unsigned int>(time(0)));
     std::cout << APP_NAME << " v" << APP_VERSION << "\n";
     MainWindow main_window;
     main_window.show();
