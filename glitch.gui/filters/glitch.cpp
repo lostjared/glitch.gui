53393dd1a3a2ccbd80173ac35189377f5646b768
Jared Bruni <jaredbruni@gmail.com>
2023-08-03 08:40:36 -0700

    x5

--- glitch.gui/filters/glitch.cpp
+++ glitch.gui/filters/glitch.cpp
@@ -89,6 +89,7 @@ void Glitch_Line_Down_X4::init() {
 
 }
 void Glitch_Line_Down_X4::proc(cv::Mat &frame) {
+
     for(int x = 0; x < frame.cols; ++x) {
         color = cv::Vec3b(rand()%255, rand()%255, rand()%255);
         for(int y = 0; y < offset_y && y < frame.rows; ++y) {
@@ -113,11 +114,32 @@ Glitch_Line_Down_X4::~Glitch_Line_Down_X4() {}
 /* line down x5 */
 
 void Glitch_Line_Down_X5::init() {
-
+    offset_y = 0;
+    color = cv::Vec3b(0, 0, 0);
 }
 
 void Glitch_Line_Down_X5::proc(cv::Mat &frame) {
+    int width=5+rand()%50;
+    for(int x2 = 0; x2 < frame.cols; x2 += width) {
+        for(int x = x2; x < x2+width && x < frame.cols; ++x) {
+            color = cv::Vec3b(rand()%255, rand()%255, rand()%255);
+            for(int y = 0; y < offset_y && y < frame.rows; ++y) {
+                cv::Vec3b &pixel = frame.at<cv::Vec3b>(y, x);
+                pixel[0] += pixel[0]^color[0];
+                pixel[1] += pixel[1]^color[1];
+                pixel[2] += pixel[2]^color[2];
+            }
+            for(int y = offset_y; y < frame.rows; ++y) {
+                cv::Vec3b &pixel = frame.at<cv::Vec3b>(y, x);
+                pixel[0] = pixel[0]|color[0];
+                pixel[1] = pixel[1]|color[1];
+                pixel[2] = pixel[2]|color[2];
+            }
+        }
+        width=5+rand()%50;
 
+    }
+    offset_y = rand()%frame.rows;
 }
 
 void Glitch_Line_Down_X5::clear() {}
