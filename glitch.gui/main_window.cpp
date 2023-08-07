f067eda43662551b5381fc79826c61530f103191
Jared Bruni <jaredbruni@gmail.com>
2023-08-07 10:59:09 -0700

    channels info

--- glitch.gui/main_window.cpp
+++ glitch.gui/main_window.cpp
@@ -174,7 +174,7 @@ void MainWindow::loadCategory(int index) {
 QString MainWindow::contentData(const std::string &fn, const cv::Mat &frame) {
     QString text;
     QTextStream stream(&text);
-    stream << "path [" << fn.c_str() << "]" << "\n info [" << frame.cols << "x" << frame.rows << "]\n";
+    stream << "path [" << fn.c_str() << "]" << "\n info [" << frame.cols << "x" << frame.rows << " channels: " << frame.channels() << "]\n";
     return text;
 }
 
