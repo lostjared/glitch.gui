1aeb1123d34a52ef31c76d8bb1c5b443dbd24838
Jared Bruni <jaredbruni@gmail.com>
2023-08-20 11:11:41 -0700

    added to list

--- glitch.gui/filters/filters.cpp
+++ glitch.gui/filters/filters.cpp
@@ -31,4 +31,5 @@ void init_filters_local() {
     new_filter_list.push_back({"New_Glitch_Mirror_Slice_Inc_X2", new Glitch_Mirror_Slice_Inc_X2()});
     new_filter_list.push_back({"New_Glitch_Mirror_Slice_X2", new Glitch_Mirror_Slice_Inc_X2()});
     new_filter_list.push_back({"New_Glitch_Mirror_Bars", new Glitch_Mirror_Bars()});
+    new_filter_list.push_back({"New_Glitch_Mirror_Bars_X2", new Glitch_Mirror_Bars_X2()});
 }
