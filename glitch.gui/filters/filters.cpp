50b5390dcee69cc2c2e309bdfabf62cbab971498
Jared Bruni <jaredbruni@gmail.com>
2023-08-27 15:59:31 -0700

    init

--- glitch.gui/filters/filters.cpp
+++ glitch.gui/filters/filters.cpp
@@ -37,5 +37,7 @@ void init_filters_local() {
     new_filter_list.push_back({"New_Glitch_Mirror_Bars_Col_OrigY", new Glitch_Mirror_Bars_Col_OrigY()});
     new_filter_list.push_back({"New_Glitch_Mirror_Bars_Col_OrigYH", new Glitch_Mirror_Bars_Col_OrigYH()});
     new_filter_list.push_back({"New_Glitch_Mirror_Bars_Col_OrigYH_Pixels", new Glitch_Mirror_Bars_Col_OrigYH_Pixels()});
+    new_filter_list.push_back({"New_Glitch_Mirror_Bars_Col_OrigY_Grad", new Glitch_Mirror_Bars_Col_OrigY_Grad()});
+
 }
 
