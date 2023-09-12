ca1ea63d8c951dedb28183681495cf9af0c17388
Jared Bruni <jaredbruni@gmail.com>
2023-09-12 05:45:35 -0700

    SizeX

--- glitch.gui/filters/glitch.cpp
+++ glitch.gui/filters/glitch.cpp
@@ -1571,3 +1571,67 @@ void Glitch_Rect_SizeY::clear() {
 
 
 Glitch_Rect_SizeY::~Glitch_Rect_SizeY() {}
+
+/* glitch rect Size X */
+
+void Glitch_Rect_SizeX::init() {
+    num_rows = 8;
+    num_cols = 8;
+    num_dir = 1;
+}
+
+void Glitch_Rect_SizeX::proc(cv::Mat &frame) {
+    collection.shiftFrames(frame);
+    int row_size = frame.rows/num_rows;
+    int col_size = frame.cols/num_cols;
+    int index = 0;
+
+    double alpha_inc = 1.0/double(num_rows);
+    double alpha_y = 1.0;
+        
+    for(int y = 0; y < frame.rows; y += row_size) {
+
+        for(int x = 0; x < frame.cols; x += col_size) {
+            double alpha = 0.1 + alpha_y;
+            drawBlock(alpha, x, y, col_size, row_size, frame, collection[index]);
+
+            index ++;
+            if(index > static_cast<int>(collection.count())-1)
+                index = 0;
+
+       alpha_y -= alpha_inc;
+
+        }   
+    }
+    if(num_dir == 1) {
+        num_rows += 2;
+        num_cols += 2;
+        if(num_rows >= 64) {
+            num_dir = 0;
+        }
+    } else {
+        num_rows -= 2;
+        num_cols -= 2;
+        if(num_rows <= 4) {
+            num_dir = 1;
+        }
+    }
+}
+
+void Glitch_Rect_SizeX::drawBlock(double &alpha, int x, int y,  int w, int h, cv::Mat &frame, const cv::Mat &src) {
+    for(int z = y; z < y+h && z < frame.rows; ++z) {
+        for(int i = x;  i < x+w && i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            const cv::Vec3b &pix = src.at<cv::Vec3b>(z, i);
+            for(int q = 0; q < 3; ++q)
+                pixel[q] = ac::wrap_cast((alpha * pixel[q]) + ((1-alpha)*pix[q]));
+        }
+    }
+}
+
+void Glitch_Rect_SizeX::clear() {
+    collection.clear();
+}
+
+
+Glitch_Rect_SizeX::~Glitch_Rect_SizeX() {}
--- glitch.gui/filters/glitch.hpp
+++ glitch.gui/filters/glitch.hpp
@@ -434,4 +434,18 @@ private:
     void drawBlock(double &alpha, int x, int y,  int w, int h, cv::Mat &frame, const cv::Mat &src);
 };
 
+class Glitch_Rect_SizeX: public FilterFunc {
+public:
+    void init() override;
+    void proc(cv::Mat &frame) override;
+    void clear() override;
+    ~Glitch_Rect_SizeX();
+private:
+    FrameCollection collection;
+    int num_rows;
+    int num_cols;
+    int num_dir;
+    void drawBlock(double &alpha, int x, int y,  int w, int h, cv::Mat &frame, const cv::Mat &src);
+};
+
 #endif
