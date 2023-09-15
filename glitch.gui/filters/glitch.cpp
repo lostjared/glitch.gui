b258bfc6c4fdf96f871fd8ea9c37f73f9fdf12b2
Jared Bruni <jaredbruni@gmail.com>
2023-09-15 01:28:31 -0700

    Size_overflow

--- glitch.gui/filters/glitch.cpp
+++ glitch.gui/filters/glitch.cpp
@@ -1645,15 +1645,14 @@ void Glitch_Rect_Size_Odd::init() {
 void Glitch_Rect_Size_Odd::proc(cv::Mat &frame) {
     collection.shiftFrames(frame);
     int row_size = frame.rows/num_rows;
-    int col_size = frame.cols/num_cols;
-    int index = 0;
-    
+    int col_size = frame.cols/num_cols;  
     for(int y = 0; y < frame.rows; y += row_size) {
         int index = rand()%(collection.count()-1);    
         for(int x = 0; x < frame.cols; x += col_size) {
             double alpha = 0.5;
             drawBlock(alpha, x, y, col_size, row_size, frame, collection[index]);   
                index ++;
+
             if(index > static_cast<int>(collection.count())-1)
                 index = 0;
          
@@ -1691,3 +1690,54 @@ void Glitch_Rect_Size_Odd::clear() {
 
 
 Glitch_Rect_Size_Odd::~Glitch_Rect_Size_Odd() {}
+
+/* glitch rect Size Overflow */
+
+void Glitch_Rect_Size_Overflow::init() {
+    num_rows = 8;
+    num_cols = 8;
+    num_dir = 1;
+}
+
+void Glitch_Rect_Size_Overflow::proc(cv::Mat &frame) {
+    collection.shiftFrames(frame);
+    int row_size = frame.rows/num_rows;
+    int col_size = frame.cols/num_cols;
+    for(int y = 0; y < frame.rows; y += row_size) {
+        for(int x = 0; x < frame.cols; x += col_size) {
+            double alpha = 1.5;
+            drawBlock(alpha, x, y, col_size, row_size, frame, collection[rand()%collection.count()]);
+        }
+    }
+    if(num_dir == 1) {
+        num_rows += 2;
+        num_cols += 2;
+        if(num_rows >= 64) {
+            num_dir = 0;
+        }
+    } else {
+        num_rows -= 2;
+        num_cols -= 2;
+        if(num_rows <= 4) {
+            num_dir = 1;
+        }
+    }
+}
+
+void Glitch_Rect_Size_Overflow::drawBlock(double &alpha, int x, int y,  int w, int h, cv::Mat &frame, const cv::Mat &src) {
+    for(int z = y; z < y+h && z < frame.rows; ++z) {
+        for(int i = x;  i < x+w && i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            const cv::Vec3b &pix = src.at<cv::Vec3b>(z, i);
+            for(int q = 0; q < 3; ++q)
+                pixel[q] = ac::wrap_cast((alpha * pixel[q]) + ((1-alpha)*pix[q]));
+        }
+    }
+}
+
+void Glitch_Rect_Size_Overflow::clear() {
+    collection.clear();
+}
+
+
+Glitch_Rect_Size_Overflow::~Glitch_Rect_Size_Overflow() {}
