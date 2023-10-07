b7aa68f57d187e2dac51acb25e081d2c646402a7
Jared Bruni <jaredbruni@gmail.com>
2023-10-07 00:00:41 -0700

    Rect size rand

--- glitch.gui/filters/glitch.cpp
+++ glitch.gui/filters/glitch.cpp
@@ -2347,3 +2347,47 @@ void Glitch_Line_Effect_Down::proc(cv::Mat &frame) {
 void Glitch_Line_Effect_Down::clear() {}
 
 Glitch_Line_Effect_Down::~Glitch_Line_Effect_Down() {}
+
+/* glitch rect Size Rand */
+
+void Glitch_Rect_Size_Rand::init() {
+    num_rows = 8;
+    num_cols = 8;
+    num_dir = 1;
+}
+
+void Glitch_Rect_Size_Rand::proc(cv::Mat &frame) {
+    collection.shiftFrames(frame);
+    int row_size = frame.rows/num_rows;
+    int col_size = frame.cols/num_cols;
+    for(int y = 0; y < frame.rows; y += row_size) {
+        for(int x = 0; x < frame.cols; x += col_size) {
+            double alpha = 0.5;
+            drawBlock(alpha, x, y, col_size, row_size, frame, collection[rand()%collection.count()]);
+        }
+    }
+    num_rows += 2;
+    num_cols += 2;
+    if(num_rows >= 64) {
+        num_rows = 1+(rand()%64);
+        num_cols = 1+(rand()%64);
+    }
+}
+
+void Glitch_Rect_Size_Rand::drawBlock(double &alpha, int x, int y,  int w, int h, cv::Mat &frame, const cv::Mat &src) {
+    for(int z = y; z < y+h && z < frame.rows; ++z) {
+        for(int i = x;  i < x+w && i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            const cv::Vec3b &pix = src.at<cv::Vec3b>(z, i);
+            for(int q = 0; q < 3; ++q)
+                pixel[q] = ac::wrap_cast((alpha * pixel[q]) + ((1-alpha)*pix[q]));
+        }
+    }
+}
+
+void Glitch_Rect_Size_Rand::clear() {
+    collection.clear();
+}
+
+
+Glitch_Rect_Size_Rand::~Glitch_Rect_Size_Rand() {}
--- glitch.gui/filters/glitch.hpp
+++ glitch.gui/filters/glitch.hpp
@@ -640,4 +640,18 @@ private:
     double alpha;
 };
 
+class Glitch_Rect_Size_Rand : public FilterFunc {
+public:
+    void init() override;
+    void proc(cv::Mat &frame) override;
+    void clear() override;
+    ~Glitch_Rect_Size_Rand();
+private:
+    FrameCollection collection;
+    int num_rows;
+    int num_cols;
+    int num_dir;
+    void drawBlock(double &alpha, int x, int y,  int w, int h, cv::Mat &frame, const cv::Mat &src);
+};
+
 #endif
