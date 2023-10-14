2eda26fd51bc471e4640e0575d33289d6e3efa2e
Jared Bruni <jaredbruni@gmail.com>
2023-10-14 00:01:13 -0700

    V4

--- glitch.gui/filters/glitch.cpp
+++ glitch.gui/filters/glitch.cpp
@@ -2497,3 +2497,34 @@ void Glitch_Line_Effect_Down_V3::clear() {
 }
 
 Glitch_Line_Effect_Down_V3::~Glitch_Line_Effect_Down_V3() {}
+
+/* Glitch line effect Down v4*/
+
+void Glitch_Line_Effect_Down_V4::init() {
+    alpha = 1.0;
+}
+
+void Glitch_Line_Effect_Down_V4::proc(cv::Mat &frame) {
+    collection.shiftFrames(frame);
+    cv::Mat &frame2 = collection.get_frame(5);
+
+    double diff = 1.0/double(frame.rows);
+    alpha = (rand()%10) * 0.1;
+
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            const cv::Vec3b &pix = frame2.at<cv::Vec3b>(z, i);
+            for(int q = 0; q < 3; ++q) {
+                pixel[q] = ac::wrap_cast((alpha * pixel[q]) + ((1-alpha)*pix[q]));
+            }
+        }
+        alpha += diff;
+    }
+}
+
+void Glitch_Line_Effect_Down_V4::clear() {
+    collection.clear();
+}
+
+Glitch_Line_Effect_Down_V4::~Glitch_Line_Effect_Down_V4() {}
