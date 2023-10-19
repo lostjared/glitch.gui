b317134b0d980775a8a11bc249bb14fb1764513f
Jared Bruni <jaredbruni@gmail.com>
2023-10-19 00:05:51 -0700

    record window

---
+++ glitch.gui/record_window.cpp
@@ -0,0 +1,5 @@
+#include"record_window.hpp"
+
+RecordWindow::RecordWindow(QWidget *parent) : QDialog(parent) {
+   setFixedSize(640, 480);
+}
---
+++ glitch.gui/record_window.hpp
@@ -0,0 +1,13 @@
+#ifndef RECORD_WINDOW_HPP_
+#define RECORD_WINDOW_HPP_
+
+#include<QDialog>
+
+class RecordWindow : public QDialog {
+    Q_OBJECT
+public:
+    RecordWindow(QWidget *parent = 0);
+};
+
+
+#endif
