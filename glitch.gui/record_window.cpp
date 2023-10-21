be52166ab2d23c191418dccdc9fb22325499f2b1
Jared Bruni <jaredbruni@gmail.com>
2023-10-21 00:12:41 -0700

    label

--- glitch.gui/record_window.cpp
+++ glitch.gui/record_window.cpp
@@ -7,16 +7,16 @@ RecordWindow::RecordWindow(QWidget *parent) : QDialog(parent) {
 
    QLabel *ff_lbl = new QLabel(tr("FFFMpeg path:"), this);
    ff_lbl->setGeometry(25, 25, 100, 25);
-
-   ffmpeg_path = new QLineEdit(this);
+   ffmpeg_path = new QLineEdit(this); 
    ffmpeg_path->setText("ffmpeg");
    ffmpeg_path->setGeometry(25+100,25,300,25);
 
-
-   ffmpeg_type = new QComboBox(this);
-   ffmpeg_type->setGeometry(25, 75, 100, 25);
-   ffmpeg_type->addItem(tr("x264"));
-   ffmpeg_type->addItem(tr("x265"));
+    QLabel *ff_type = new QLabel(tr("Codec: "), this);
+    ff_type->setGeometry(25, 75, 50, 25);
+    ffmpeg_type = new QComboBox(this);
+    ffmpeg_type->setGeometry(25+50, 75, 100, 25);
+    ffmpeg_type->addItem(tr("x264"));
+    ffmpeg_type->addItem(tr("x265"));
 }
 
 void RecordWindow::setMainWindow(MainWindow *m) {
