4407bac31e276a5feb49a1f4fc501833145f8f92
Jared Bruni <jaredbruni@gmail.com>
2023-11-09 03:57:48 -0800

    version info

--- glitch.gui/version_info.hpp
+++ glitch.gui/version_info.hpp
@@ -1,6 +1,6 @@
 #ifndef _VERSION_INFO_H_
 #define _VERSION_INFO_H_
-#define APP_VERSION "0.4.0"
+#define APP_VERSION "0.4.1"
 #define APP_NAME "glitch.gui"
 #define AUTHOR_NAME "Jared Bruni"
 #endif
