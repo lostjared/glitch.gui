b9f495cfd8daa2c572268ef9cd1433701d9c85af
Jared Bruni <jaredbruni@gmail.com>
2023-11-13 01:34:23 -0800

    custom

--- glitch.gui/custom_window.cpp
+++ glitch.gui/custom_window.cpp
@@ -3,6 +3,11 @@
 
 CustomWindow::CustomWindow(QWidget *parent) : QDialog(parent) {
     setFixedSize(640, 480);
+    filter_cat = new QComboBox(this);
+    filter_cat->setGeometry(15, 15, (640-15-15), 25);
+    filter = new QComboBox(this);
+    filter->setGeometry(15, 15+25+5, (640-15-10), 25);
+    
 }
 
 void CustomWindow::setMainWindow(MainWindow *m) {
