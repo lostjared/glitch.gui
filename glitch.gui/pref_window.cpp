cbb876640cafbd3bac61c17ddcd50da9cf20f160
Jared Bruni <jaredbruni@gmail.com>
2023-12-16 06:26:45 -0800

    pref

--- glitch.gui/pref_window.cpp
+++ glitch.gui/pref_window.cpp
@@ -1,7 +1,8 @@
 #include"pref_window.hpp"
 
 PrefWindow::PrefWindow(QWidget *parent) : QDialog(parent) {
-
+    setFixedSize(640, 480);
+    setWindowTitle("Preferences");
 }
 
 void PrefWindow::setMainWindow(MainWindow *m) {
