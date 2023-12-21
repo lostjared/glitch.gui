e1962a62c90dcd5276862bcc1c1500eb888dd984
Jared Bruni <jaredbruni@gmail.com>
2023-12-21 07:17:41 -0800

    working on audio mux windowo

--- glitch.gui/mux_window.cpp
+++ glitch.gui/mux_window.cpp
@@ -1,6 +1,10 @@
 #include"mux_window.hpp"
+#include<QIcon>
 
 
 MuxWindow::MuxWindow(QWidget *parent) : QDialog(parent) {
     setFixedSize(640, 480);
+    setWindowIcon(QIcon(":/images/icon.png"));
+    setWindowTitle("Copy Audio");
+    
 }
