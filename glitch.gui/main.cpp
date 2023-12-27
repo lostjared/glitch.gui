697aad5d263d1f31924d5bd5da5d89c4948adc89
Jared Bruni <jaredbruni@gmail.com>
2023-12-26 19:51:36 -0800

    copyright

--- glitch.gui/main.cpp
+++ glitch.gui/main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char **argv) {
     // init random seed
     srand(static_cast<unsigned int>(time(0)));
     std::cout << APP_NAME << " v" << APP_VERSION << "\n";
+    std::cout << "(C) 2024 LostSideDead Software. GPL v3.\n";
     MainWindow main_window;
     main_window.show();
     app.exec();
