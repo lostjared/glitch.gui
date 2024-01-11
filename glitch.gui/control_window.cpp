b6e25fa7f8c03193e1b5ce771fb6ead791074338
Jared Bruni <jaredbruni@gmail.com>
2024-01-10 23:47:21 -0800

    basic window

--- glitch.gui/control_window.cpp
+++ glitch.gui/control_window.cpp
@@ -1,2 +1,11 @@
 #include"control_window.hpp"
 
+Control_Window::Control_Window(QWidget *parent) : QDialog(parent) {
+
+
+}
+
+
+void Control_Window::setMainWindow(MainWindow *m) {
+    main_window = m;
+}
--- glitch.gui/control_window.hpp
+++ glitch.gui/control_window.hpp
@@ -1,10 +1,22 @@
 #ifndef _CONTROL_WINDOW_H_
 #define _CONTROL_WINDOW_H_
 
+#include<QDialog>
+
+class MainWindow;
+
+class Control_Window : public QDialog {
+Q_OBJECT
+
+public:
+    Control_Window(QWidget *parent = 0);
+    void setMainWindow(MainWindow *m);
+
+private:
+    MainWindow *main_window;
 
 
-
-
+};
 
 
 #endif
