6432de640eb61f43418a0667ade6c057aaa98e28
Jared Bruni <jaredbruni@gmail.com>
2024-01-16 20:49:58 -0800

    disable quickedit mode on startup

--- glitch.gui/main.cpp
+++ glitch.gui/main.cpp
@@ -2,9 +2,18 @@
 #include"main_window.hpp"
 #include<ctime>
 #include<cstdlib>
-
+#ifdef _WIN32
+#include<windows.h>
+#endif
 
 int main(int argc, char **argv) {
+#ifdef _WIN32
+    HANDLE hInput;
+    DWORD prev_mode;
+    hInput = GetStdHandle(STD_INPUT_HANDLE);
+    GetConsoleMode(hInput, &prev_mode); 
+    SetConsoleMode(hInput, ENABLE_EXTENDED_FLAGS | (prev_mode & ~ENABLE_QUICK_EDIT_MODE));
+#endif
     QApplication app(argc, argv);
     // init random seed
     srand(static_cast<unsigned int>(time(0)));
@@ -13,5 +22,8 @@ int main(int argc, char **argv) {
     MainWindow main_window;
     main_window.show();
     app.exec();
+#ifdef _WIN32
+    SetConsoleMode(hInput, prev_mode);
+#endif
     return 0;
 }
