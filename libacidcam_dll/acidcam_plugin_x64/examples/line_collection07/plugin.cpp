f98e32fe42f75ea95ff29d4460b052f09fea6bc3
Jared Bruni <jaredbruni@gmail.com>
2024-01-20 05:30:14 -0800

    colleciton 07

---
+++ libacidcam_dll/acidcam_plugin_x64/examples/line_collection07/plugin.cpp
@@ -0,0 +1,49 @@
+#include "acidcam/ac.h"
+
+static constexpr int MAX = 8;
+static ac::MatrixCollection<MAX> collection;
+
+extern "C" void init() {
+    std::cout << "line_collection07 initialized...\n";
+    srand(static_cast<unsigned int>(time(0)));
+}
+
+extern "C" void rls() {
+    ac::release_all_objects();
+}
+
+extern "C" void clear() {
+    std::cout << "Clear called..\n";
+    ac::release_all_objects();
+}
+
+extern "C" void proc(cv::Mat  &frame) {
+    if(collection.empty()) {
+        srand(static_cast<unsigned int>(time(0)));
+        collection.shiftFrames(frame);
+    }
+    else
+        collection.shiftFrames(frame);
+    
+    
+    static int r_count = rand()%50;
+    
+    for(int z = 0; z < frame.rows; ++z) {
+        
+        int cnt = 0;
+        int offset = rand()%(MAX-1);
+      
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+            cv::Vec3b pix = collection.frames[offset].at<cv::Vec3b>(z, i);
+            pixel = pix;
+            
+            if(++cnt > r_count) {
+                cnt = 0;
+                r_count = rand()%50;
+                if(++offset > MAX-1)
+                    offset = 0;
+            }
+        }
+    }
+}
