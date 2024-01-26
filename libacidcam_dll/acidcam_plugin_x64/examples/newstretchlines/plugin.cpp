50493ecf90e92d2e4bdbdd779b0cac5c90970bc9
Jared Bruni <jaredbruni@gmail.com>
2024-01-25 23:35:04 -0800

    newstretchlines

---
+++ libacidcam_dll/acidcam_plugin_x64/examples/newstretchlines/plugin.cpp
@@ -0,0 +1,35 @@
+#include "acidcam/ac.h"
+
+static constexpr int MAX = 48;
+static ac::MatrixCollection<MAX> collection;
+
+extern "C" void init() {
+    std::cout << "newstretchlines initialized...\n";
+    srand(static_cast<unsigned int>(time(0)));
+}
+
+extern "C" void rls() {
+    ac::release_all_objects();
+}
+
+extern "C" void clear() {
+    std::cout << "Clear called..\n";
+    ac::release_all_objects();
+}
+
+extern "C" void proc(cv::Mat  &frame) {
+    collection.shiftFrames(frame);
+    int off = 0;
+    int max = rand()%MAX;
+    for(int z = 0; z < frame.rows; ++z) {
+        ++off;
+        if(off >= max)
+            off = 0;
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            cv::Mat &m = collection.frames[off];
+            cv::Vec3b &pix = m.at<cv::Vec3b>(z, i);
+            pixel = pix;
+        }
+    }
+}
