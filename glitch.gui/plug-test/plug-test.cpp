922a86a740a1ef74abde80e3a987c97634c0adb5
Jared Bruni <jaredbruni@gmail.com>
2024-01-27 02:03:01 -0800

    plug-test updated

--- glitch.gui/plug-test/plug-test.cpp
+++ glitch.gui/plug-test/plug-test.cpp
@@ -4,21 +4,41 @@
 #include<opencv2/opencv.hpp>
 
 int main(int argc, char **argv) {
-    if(argc == 2) {
-        QString fname = argv[1]; 
-        Plugin_Program *prog = new Plugin_Program(fname);
-        if(!prog) {
-            std::cerr << "plugin: Error on allocation..\n";
+    
+    if(argc == 3) {
+        cv::VideoCapture cap;
+        cap.open(argv[2]);
+        if(cap.isOpened()) {
+            QString fname = argv[1];
+            Plugin_Program *prog = new Plugin_Program(fname);
+            if(!prog) {
+                std::cerr << "plugin: Error on allocation..\n";
+                exit(EXIT_FAILURE);
+            }
+            prog->f_init();
+            bool active = true;
+            cv::namedWindow("plug-test");
+            while(active) {
+                cv::Mat frame;
+                if(!cap.read(frame)) {
+                    cap.open(argv[2]);
+                    if(!cap.isOpened()) {
+                        std::cerr << "Error opening video file..\n";
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                prog->f_proc(frame);
+                cv::imshow("plug-test", frame);
+                if(cv::pollKey() != 0) {
+                    active = false;
+                }
+            }
+            prog->f_clear();
+            prog->f_rls();
+            delete prog;
+        } else {
+            std::cerr << "Error capture device could not load plug.data.mp4\n";
             exit(EXIT_FAILURE);
-        }       
-        prog->f_init();
-        cv::Mat frame;
-        frame.create(240, 320, CV_8UC3);   
-        for(int i = 0; i < 25; ++i) {
-            prog->f_proc(frame);
         }
-        prog->f_clear();
-        prog->f_rls();
-        delete prog;
     }
 }
