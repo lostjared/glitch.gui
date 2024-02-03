00c95d021f3fa2a0ed2c2e6e5fc4688f12599c92
Jared Bruni <jaredbruni@gmail.com>
2024-02-03 05:08:52 -0800

    alpha fade layer 0 and layer 1

--- glitch.gui/layers_window.cpp
+++ glitch.gui/layers_window.cpp
@@ -161,6 +161,67 @@ private:
     Layer *layer_[3];
 };
 
+// fade
+
+class Layer_AlphaBlendFade01 : public FilterFunc {
+public:
+    void init() override {
+        alpha = 0.1;
+        inc = 0.01;
+        dir = 1;
+    }
+    void setLayer(Layer *layer1, Layer *layer2) {
+        layer_[0] = layer1;
+        layer_[1] = layer2;
+    }
+    void proc(cv::Mat &frame) override {
+        cv::Mat layers[2];
+        if(layer_[0]->hasNext() && layer_[1]->hasNext()) {
+            if(layer_[0]->read(layers[0]) && layer_[1]->read(layers[1])) {
+                cv::Mat resized[2];
+                cv::resize(layers[0], resized[0], frame.size());
+                cv::resize(layers[1], resized[1], frame.size());
+                for(int z = 0; z < frame.rows; ++z) {
+                    for(int i = 0; i < frame.cols; ++i) {
+                        cv::Vec3b &pix1 = frame.at<cv::Vec3b>(z, i);
+                        cv::Vec3b pix2[2];
+                        setvec(pix2[0],resized[0].at<cv::Vec3b>(z, i));
+                        setvec(pix2[1],resized[1].at<cv::Vec3b>(z, i));
+                        for(int q = 0; q < 3; ++q) {
+                            unsigned char value = cv::saturate_cast<uchar>((0.5 * pix2[0][q]) + (0.5 * pix2[1][q]));
+                            pix1[q] = cv::saturate_cast<uchar>((alpha * pix1[q]) + ((1-alpha) * value));
+                        }
+                    }
+                }
+            }
+        }
+        if(dir == 1) {
+            alpha += inc;
+            if(alpha >= 1.0) {
+                alpha = 1.0;
+                dir = 0;
+            }
+        } else {
+            alpha -= inc;
+            if(alpha <= 0.1) {
+                alpha = 0.1;
+                dir = 1;
+            }
+        }
+    }
+    void clear() override {
+
+    }
+    ~Layer_AlphaBlendFade01() {
+
+    }
+private:
+    Layer *layer_[2];
+    double alpha = 0.1;
+    double inc = 0.01;
+    int dir = 1;
+};
+
 
 bool checkThere(std::string src, const std::vector<std::string> &keys) {
 
@@ -376,8 +437,13 @@ LayersWindow::LayersWindow(QWidget *parent) : QDialog(parent) {
     layer012_blend->setLayer(&layer1, &layer2, &layer3);
     new_filter_list.push_back({"New_Layer_012_AlphaBlend", layer012_blend});
 
+    // Fade
+    Layer_AlphaBlendFade01 *layer01_fade = new Layer_AlphaBlendFade01();
+    layer01_fade->setLayer(&layer1, &layer2);
+    new_filter_list.push_back({"New_Layer_01_Fade", layer01_fade});
+
     connect(layer_index, SIGNAL(currentIndexChanged(int)), this, SLOT(setIndexLayer(int)));
-    layer_text->setText("[Slot Closed]");
+    layer_text->setText("[Slot Closed");
 }
 
 void LayersWindow::setLayer() {
