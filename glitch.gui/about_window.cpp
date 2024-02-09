3cee18e6a173109e8477de552805639f4cf5dbb2
Jared Bruni <jaredbruni@gmail.com>
2024-02-09 00:46:00 -0800

    init files

---
+++ glitch.gui/about_window.cpp
@@ -0,0 +1,6 @@
+#include"about_window.hpp"
+
+AboutWindow::AboutWindow(QWidget *parent) : QDialog(parent) {
+    setFixedSize(640, 480);
+
+}
---
+++ glitch.gui/about_window.hpp
@@ -0,0 +1,13 @@
+#ifndef __ABOUT_WINDOW_H__
+#define __ABOUT_WINDOW_H__
+
+#include<QDialog>
+
+class AboutWindow : public QDialog {
+    Q_OBJECT
+public:
+    AboutWindow(QWidget *parent = 0);
+};
+
+
+#endif
