08c519ca6855d7eb2cbc342f93507771d1a479ce
Jared Bruni <jaredbruni@gmail.com>
2024-02-25 13:36:43 -0800

    lfo

---
+++ glitch.gui/lfo.hpp
@@ -0,0 +1,35 @@
+#ifndef _LFO_H__P
+#define _LFO_H__P
+
+#include <cmath>
+
+class LFO {
+public:
+    LFO(float frequency, float sampleRate) : mFrequency(frequency), mSampleRate(sampleRate) {
+        updateIncrement();
+    }
+    void setFrequency(float frequency) {
+        mFrequency = frequency;
+        updateIncrement();
+    }
+    float nextSample() {
+        float sample = sinf(mPhase);
+        mPhase += mPhaseIncrement;
+        if (mPhase >= 2.0f * M_PI) {
+            mPhase -= 2.0f * M_PI;
+        }
+        return sample;
+    }
+
+private:
+    float mFrequency;
+    float mSampleRate;
+    float mPhase = 0.0f;
+    float mPhaseIncrement;
+
+    void updateIncrement() {
+        mPhaseIncrement = (2.0f * M_PI * mFrequency) / mSampleRate;
+    }
+};
+
+#endif
