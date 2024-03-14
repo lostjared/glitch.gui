238bf4587a57e86b4bc1dcf233aac8da36bf68bd
Jared Bruni <jaredbruni@gmail.com>
2024-03-13 21:14:57 -0700

    palette

--- glitch.gui/cv_fractal.hpp
+++ glitch.gui/cv_fractal.hpp
@@ -41,6 +41,10 @@ namespace cv_fract {
             zoom_ = z;
         }
 
+        void resetPallette() {
+            loadPalette();            
+        }
+
         void draw(cv::Mat &frame) {
              const double aspect_ratio = static_cast<double>(frame.cols) / frame.rows;
              const double range_real = 4.0 / zoom_; 
