42d46dc2a98b5338a7381b9f61755f1e613540ee
Jared Bruni <jaredbruni@gmail.com>
2024-03-23 00:30:26 -0700

    scoped

--- glitch.gui/main.cpp
+++ glitch.gui/main.cpp
@@ -11,24 +11,43 @@
 QSettings settings("LostSideDead", "glitch.gui");
 std::string current_path;
 
+
+class ScopedRAII {
+public:
+    ScopedRAII() {
+        std::cout << "Setting Console Mode Settings...\n";
+#ifdef _WIN32
+        hInput = GetStdHandle(STD_INPUT_HANDLE);
+        GetConsoleMode(hInput, &prev_mode); 
+        SetConsoleMode(hInput, ENABLE_EXTENDED_FLAGS | (prev_mode & ~ENABLE_QUICK_EDIT_MODE));
+#endif
+    }
+    ~ScopedRAII() {
+        std::cout << "Restoring Console Settings...\n";
+#ifdef _WIN32
+        SetConsoleMode(hInput, prev_mode);
+#endif
+    }
+private:
+#ifdef _WIN32
+    HANDLE hInput; // resources
+    DWORD prev_mode;  
+#endif
+};
+
+
 int main(int argc, char **argv) {
 #ifdef __APPLE__
     current_path = argv[0];
     current_path = current_path.substr(0, current_path.rfind("/"));
     current_path += "/../Resources/";
 #endif
-#ifdef _WIN32
-    HANDLE hInput;
-    DWORD prev_mode;
-    hInput = GetStdHandle(STD_INPUT_HANDLE);
-    GetConsoleMode(hInput, &prev_mode); 
-    SetConsoleMode(hInput, ENABLE_EXTENDED_FLAGS | (prev_mode & ~ENABLE_QUICK_EDIT_MODE));
-#endif
     QApplication app(argc, argv);
     // init random seed
     srand(static_cast<unsigned int>(time(0)));
     std::cout << APP_NAME << " v" << APP_VERSION << "\n";
     std::cout << APP_YEAR << " LostSideDead Software. GPL v3.\nLoading...\n";
+    ScopedRAII scopedConsoleMode;
     ac::init();
     ac::setMaxAllocated(325);
     std::cout << "libacidcam v" << ac::version << " succesfully loaded...\n";
@@ -37,10 +56,6 @@ int main(int argc, char **argv) {
     MainWindow main_window;
     main_window.show();
     app.exec();
-#ifdef _WIN32
-    SetConsoleMode(hInput, prev_mode);
-#endif
     std::cout << "glitch-gui: Shutdown Successful... Exiting \n";
-
-    return 0;
+   return 0;
 }
--- glitch.gui/version_info.hpp
+++ glitch.gui/version_info.hpp
@@ -1,7 +1,7 @@
 #ifndef _VERSION_INFO_H_
 #define _VERSION_INFO_H_
 #define APP_VERSION "0.37.0"
-#define APP_NAME "glitch.gui"
+#define APP_NAME "Glitch.GUI"
 #define AUTHOR_NAME "Jared Bruni"
 #define APP_YEAR "(C) 2023-2024"
 #endif
